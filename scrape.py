218ae648c96d6a36815eb9bd43a147e65175e860
vinny <vzhangzh@uwaterloo.ca>
2017-09-14 20:18:00 -0400

    Fixed image/submission

--- scrape.py
+++ scrape.py
@@ -2,17 +2,16 @@ import praw
 import time
 import logging
 import configparser
-from submission import *
+from peewee import *
 
-
-def fetch(sub):
-    logging.info('Fetching from: ' + sub.display_name)
-    db = SqliteDatabase(sub.display_name + '.db')
+def fetch(sub, db):
     db.connect()
     if (not sub.display_name + '.lastfetch' in config['custom']):
         config['custom'][sub.display_name + '.lastfetch'] = '1300000000'
-
-    db.create_tables([Submission,ImageSubmission])
+    if not Submission.table_exists():
+        db.create_table(Submission)
+    if not ImageSubmission.table_exists():
+        db.create_table(ImageSubmission)
     db.close()
     imagePosts = []
     posts = []
@@ -36,15 +35,15 @@ def fetch(sub):
             slen += 1
         size += 1
 
-        if ilen % 500 == 0:
+        if ilen != 0 and ilen % 500 == 0:
             with db.atomic():
                 for dex in range(ilen - 500, len(imagePosts), 500):
                     ImageSubmission.insert_many(posts[dex:dex + 500]).execute()
                 logger.info('updated image table')
-        if slen % 500 == 0:
+        if slen != 0 and slen % 500 == 0:
                 for dex in range(size - 500, len(posts), 500):
                     Submission.insert_many(posts[dex:dex + 500]).execute()
-                logger.info('updated image table')
+                logger.info('updated submission table')
         if size % 1000 == 0:
             logger.info('fetched ' + size.__str__() + ' posts')
 
@@ -65,4 +64,22 @@ logging.info('Authenticated as ' + reddit.user.me().__str__())
 
 subb = reddit.subreddit(config['custom']['sub'].lower())
 
-fetch(subb)
+logging.info('Fetching from: ' + subb.display_name)
+db = SqliteDatabase(subb.display_name + '.db')
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Submission(BaseModel):
+    uid = CharField()
+    flair = CharField()
+    title = CharField()
+    url = CharField()
+    body = CharField()
+
+class ImageSubmission(Submission):
+    image = CharField()
+
+fetch(subb, db)
+
